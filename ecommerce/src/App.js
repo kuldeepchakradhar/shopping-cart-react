76b1237da350a40e1da41ba09e81b4805c2e720e
Arun <arun@gocoop.com>
2020-05-15 17:28:58 +0530

    routing is done

--- ecommerce/src/App.js
+++ ecommerce/src/App.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import './App.css';
+import { Switch, Route } from 'react-router-dom';
 import Homepage from './pages/homepage/homepage.components';
 
+
+  const hatspage = () => (
+    <div>
+      <h3>Hats page</h3>
+    </div>
+  );
+
+  const Mens = () => (
+    <div>
+      <h3>Mens wear</h3>
+    </div>
+  );
 function App() {
   return (
     <div>
-      <Homepage />
+      <Switch>
+      <Route exact path="/" component={Homepage} />
+      <Route  path="/hats" component={hatspage} />
+      <Route path="/mens" component={Mens} />
+      </Switch>
     </div>
   );
 }
